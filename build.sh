b8b59253aad42e39ba4bd439634dfa5c1fca8cd9
Corwin McKnight <corwin.mcknight@gmail.com>
2020-02-07 11:46:21 -0800

    build: Allow DIR_BUILD to be set on command line

--- build.sh
+++ build.sh
@@ -1,18 +1,28 @@
+# Set build directory
+if [ -z "$DIR_BUILD" ]
+then
+    DIR_BUILD=./build
+fi
+DIR_BUILD=$(readlink -f $DIR_BUILD)
+mkdir -p $DIR_BUILD
+
+# Start compiling projects
 cd projects
 
 cd awd
-make awd -j16
+DIR_BUILD=$DIR_BUILD make awd -j4
 cd ..
 
 cd lobo
-make lobo -j16
+DIR_BUILD=$DIR_BUILD make lobo -j4
 cd ..
 
 cd .. # Projects
 
-mkdir -p build/sysroot/
-cp -r data/iso/* build/sysroot/
-cp build/awd/awd.elf build/sysroot/boot.elf
-cp build/lobo/lobo.elf build/sysroot/kernel.elf
+# Build ISO
+mkdir -p $DIR_BUILD/sysroot/
+cp -r data/iso/* $DIR_BUILD/sysroot/
+cp $DIR_BUILD/awd/awd.elf $DIR_BUILD/sysroot/boot.elf
+cp $DIR_BUILD/lobo/lobo.elf $DIR_BUILD/sysroot/kernel.elf
 
-grub-mkrescue -o build/bootable.iso build/sysroot/ &> /dev/null
+grub-mkrescue -o $DIR_BUILD/bootable.iso $DIR_BUILD/sysroot/ &> /dev/null
